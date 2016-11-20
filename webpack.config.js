4e8f7865112e71b301ff7bdf6c485f2e1f7a1c26
Mayur Gaud <mayur_gaud99@yahoo.com>
2016-11-20 20:12:26 +0100

    Created the boilerplate of app

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require("path");
+var webpack = require("webpack");
+ 
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080', // WebpackDevServer host and port
+    'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
+    './src/index' // Your appʼs entry point
+  ],
+  output: {
+    path: path.resolve(__dirname, './build'),
+    publicPath: '/build/',
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: ['babel']
+      }
+    ]
+  }
+};
