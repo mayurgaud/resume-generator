913361b51ed9a69c9fc4468a63d66438a72e04cb
Mayur Gaud <mayur_gaud99@yahoo.com>
2016-11-22 13:35:43 +0100

    Added the content

--- src/components/app/experience/ExperienceColumn.jsx
+++ src/components/app/experience/ExperienceColumn.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import s from './Experience.scss';
 
 class ExperienceColumn extends Component {
@@ -6,7 +6,252 @@ class ExperienceColumn extends Component {
     render() {
         return (
             <div className="row">
-                experience column
+                <div className="main">
+                    <section id="summary" className="text_story">
+                        <header>
+                            <h3 className="header-text">
+                                <span className="undefined needsclick">Summary</span>
+                            </h3>
+                        </header>
+                        <article>
+                            <div id="react-tinymce-4" className=" mce-content-body">
+                                <p>
+                                    <strong>Innovative
+                                        IT professional</strong> offering vast experience leveraging software
+                                    engineering
+                                    and
+                                    DevOps methodologies to deliver highly effective and creative solutions to business
+                                    and
+                                    technology challenges. Utilizes highly attuned analytical skills to develop IT and
+                                    business strategies employing cutting-edge technologies to increase productivity.
+                                    Consistently drives high standards of service through effective project management,
+                                    communication, and strategic planning to develop and manage strong client
+                                    relationships.
+                                    Highly organized with strong capacity to prioritize workload, delegate deliverables,
+                                    and
+                                    steer project completion within established deadlines.<br/><br/>
+                                    <span><strong>Core Competencies</strong></span>
+                                </p>
+                                <table className="mce-item-table">
+                                    <tbody>
+                                    <tr>
+                                        <td>
+                                            <ul>
+                                                <li>DevOps</li>
+                                                <li>Techlead</li>
+                                                <li>Start-ups / Entrepreneurship</li>
+                                                <li>Software Development / Engineering</li>
+                                                <li>Team Building / Leadership</li>
+                                            </ul>
+                                        </td>
+                                        <td>
+                                            <ul>
+                                                <li>Training &amp; Mentoring</li>
+                                                <li>Budget Management</li>
+                                                <li>Outsourcing / Offshoring</li>
+                                                <li>Software Life Cycle Management, LCM</li>
+                                                <li>Software Change / Configuration Management, SCCM</li>
+                                                <li><span>Project Management</span>
+                                                </li>
+                                            </ul>
+                                        </td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </article>
+                    </section>
+                    <section id="positions" className="dated_story"
+                    >
+
+                        <header><h3 className="header-text"><span
+                            className="undefined needsclick">Work History</span></h3></header>
+                        <article >
+                            <div className="article-headings"><h6
+                                className="date-range"
+                            >
+                                <span><span
+                                    className="undefined needsclick">Jun 2015</span></span><span
+                            ><span
+                                className="undefined needsclick">Present</span></span></h6></div>
+                            <div className="article-body"><h4 >
+                                <span className="undefined needsclick">Tech lead</span></h4>
+                                <div><h5 >
+                                    <span className="undefined needsclick">Ansh Systems Pvt. Ltd.</span>
+                                </h5></div>
+                                <div id="react-tinymce-5" className=" mce-content-body"><p>Successfully established and
+                                    built software engineering and development company providing solutions to a variety
+                                    of companies’ business and operational needs. Led complex software development for a
+                                    broad range of functions including invoice management systems, e-commerce, and sales
+                                    systems.</p>
+                                    <ul>
+                                        <li>Built a solid reputation for delivering high-quality, cost-efficient
+                                            technological solutions and tools driving business development and the
+                                            establishment of key partnerships with large organizations and Fortune 500
+                                            firms that included IBM and HP.
+                                        </li>
+                                        <li>Managed all aspects of business and project development from initial scoping
+                                            through strategic and fiscal planning to final testing and delivery.
+                                        </li>
+                                        <li>Delivered outsourced service to IKEA IT maintaining support, development and
+                                            life cycle management of IBM Rational tools for change and configuration and
+                                            Oracle development tools, including all products/services source code and
+                                            changes.
+                                        </li>
+                                        <li>Invited to take over software change and configuration management tools
+                                            project supporting 2000 IKEA IT users, successfully learning everything from
+                                            the ground up.
+                                        </li>
+                                        <li>Managed development for IKEA for the development tools group along with
+                                            setting up the VMware ESX environment tech support Mercury Quality Center
+                                            (HP ALM).
+                                        </li>
+                                        <li>Currently lead the management of IKEA’S CLM (IBM) suite including tools such
+                                            as Rational Team Concert, Rational Requirements Composer, and Rational
+                                            Quality.
+                                        </li>
+                                        <li>Subject Matter expert regarding source code configuration management,
+                                            process engineering, change management, and parallel development for Swedish
+                                            National Police using ClearCase (Base/UCM), Quality Center.
+                                        </li>
+                                        <li>Led DevOps awareness inside IKEA organization, established thru vendors like
+                                            Microsoft, HP, IBM and CA.
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </article>
+                        <article>
+
+                            <div className="article-headings"><h6
+                                className="date-range"
+                            >
+                                <span ><span
+                                    className="undefined needsclick">Jan 2014</span></span><span
+                            ><span
+                                className="undefined needsclick">Apr 2015</span></span></h6></div>
+                            <div className="article-body"><h4 >
+                                <span className="undefined needsclick">Lead Developer</span></h4>
+                                <div ><h5 >
+                                    <span className="undefined needsclick">Splattered Ink, Murphysboro IL (U.S)</span>
+                                </h5></div>
+                                <div id="react-tinymce-6" className=" mce-content-body"><p>Selected from 400 candidates
+                                    to lead software development for a new platform for electronic invoicing management,
+                                    utilizing MS Windows, C#, ASP,. NET, SQL Server 2000, UML, RUP, WSE, Webservices and
+                                    XML, a new electronic invoicing system for company's finance and accounting
+                                    processes.</p>
+                                    <ul>
+                                        <li>Performed detailed analysis of business requirements and developed
+                                            comprehensive plans defining and scoping project requirements.
+                                        </li>
+                                        <li>Built open source joomla&nbsp;components, modules and plugins.</li>
+                                        <li>Established clear timeline and monitoring systems to successfully manage
+                                            project delivery within agreed-upon time frame and budget.
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </article>
+                        <article >
+
+                            <div className="article-headings"><h6
+                                className="date-range"
+                            >
+                                <span ><span
+                                    className="undefined needsclick"
+
+                                >Jun 2012</span></span><span
+                            ><span
+                                className="undefined needsclick"
+                            >Dec 2013</span></span></h6></div>
+                            <div className="article-body"><h4>
+                                <span className="undefined needsclick">Software Engineer</span></h4>
+                                <div><h5>
+                                    <span className="undefined needsclick">DRC Systems Pvt. Ltd.</span>
+                                </h5></div>
+                                <div id="react-tinymce-7" className=" mce-content-body">
+                                    <ul>
+                                        <li>Successfully led team of developers on Siebel software project for
+                                            e-auctions.
+                                        </li>
+                                        <li>Established new office as part of company expansion, playing a key role in
+                                            defining strategy and planning business processes.
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </article>
+                    </section>
+                    <section >
+                        <header><h3 className="header-text"><span
+                            className="undefined needsclick">Education</span></h3></header>
+                        <article >
+
+                            <div className="article-headings"><h6
+                                className="date-range"
+                            >
+                                <span ><span
+                                    className="undefined needsclick">2008</span></span><span
+                            ><span
+                                className="undefined needsclick">2012</span></span></h6></div>
+                            <div className="article-body"><h4 >
+                                <span className="undefined needsclick"
+
+                                >Bachelor of Engineering (I.T)</span>
+                            </h4>
+                                <div ><h5
+                                >
+                                    <span className="undefined needsclick">Trinity institute of technology &amp;
+                                        research</span></h5></div>
+
+                            </div>
+                        </article>
+                        <article>
+                            <div className="article-headings">
+                                <h6 className="date-range">
+                                <span>
+                                    <span
+                                        className="undefined needsclick">2007</span></span><span
+                                ><span
+                                    className="undefined needsclick"
+                                >2008</span></span></h6></div>
+                            <div className="article-body"><h4>
+                                <span className="undefined needsclick">Senior Secondary</span></h4>
+                                <div>
+                                    <h5>
+                                        <span className="undefined needsclick">Bhe. Dr Radhakrishnan Senior Sec. School C.B.S.E</span>
+                                    </h5></div>
+                                <div id="react-tinymce-9" className=" mce-content-body empty">
+                                </div>
+                            </div>
+                        </article>
+                        <article>
+                            <div className="article-headings">
+                                <h6 className="date-range">
+                                <span>
+                                    <span className="undefined needsclick">2005</span>
+                                </span>
+                                    <span>
+                                        <span className="undefined needsclick">2006</span>
+                                    </span>
+                                </h6>
+                            </div>
+                            <div className="article-body">
+                                <h4>
+                                    <span className="undefined needsclick">Higher Secondary</span>
+                                </h4>
+                                <div target="_blank" rel="nofollow">
+                                    <h5>
+                                        <span className="undefined needsclick">S.I.C.A Senior Sec.School C.B.S.E</span>
+                                    </h5>
+                                </div>
+                                <div id="react-tinymce-10" className=" mce-content-body empty">
+                                    <p><br/></p>
+                                </div>
+                            </div>
+                        </article>
+                    </section>
+                </div>
             </div>
         );
     }
--- src/components/app/skill/SkillColumn.jsx
+++ src/components/app/skill/SkillColumn.jsx
@@ -1,7 +1,11 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import s from './Skill.scss';
 
 class SkillColumn extends Component {
+
+    constructor(props) {
+        super(props);
+    }
 
     render() {
         return (
@@ -9,7 +13,8 @@ class SkillColumn extends Component {
                 <section id="profile" className="profile">
                     <article className="vcard">
                         <div className="user-thumb">
-                            <img className="css-crop bg-cover" src="https://workstory.s3.amazonaws.com/assets/1297759/IMG_20160809_140403_cv.jpg"/>
+                            <img className="css-crop bg-cover"
+                                 src="https://workstory.s3.amazonaws.com/assets/1297759/IMG_20160809_140403_cv.jpg"/>
                         </div>
                         <div className="fn user-title">
                             <h1 className="full-name">
@@ -30,6 +35,81 @@ class SkillColumn extends Component {
                                 <span className="undefined needsclick">contact@mayurgaud.com</span>
                             </li>
                         </ul>
+                    </article>
+                </section>
+                <section id="strengths" className="strengths">
+                    <div className="edit-btn edit-section hidden-xs hidden-sm">
+                        <span className="add-article-btn">
+                            <i className="fa fa-plus"/>
+                            <span></span>
+                            <span>Skills</span>
+                        </span>
+                        <i className="fa fa-arrow-up fa-fw"/>
+                        <i className="fa fa-arrow-down fa-fw"/>
+                        <i className="fa fa-trash-o fa-fw"/>
+                    </div>
+                    <header>
+                        <h3 className="header-text">
+                            <span className="undefined needsclick">Skills</span>
+                        </h3>
+                    </header>
+                    <article className="competency">
+                        <div className="article-headings">
+                            <h6>
+                                <span className="undefined needsclick">Front End</span>
+                            </h6>
+                        </div>
+                        <div className="article-body competency">
+                            <div id="react-tinymce-1" className=" mce-content-body">
+                                <ul>
+                                    <li>Javascript (ES5/ES6)</li>
+                                    <li>jQuery, Ajax</li>
+                                    <li>HTML5</li>
+                                    <li>ReactJs</li>
+                                    <li>Extjs</li>
+                                    <li>Bootstrap, Foundation</li>
+                                    <li>CSS3, Sass, Less</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="clearfix"></div>
+                    </article>
+                    <article className="competency">
+                        <div className="article-headings">
+                            <h6>
+                                <span className="undefined needsclick">Back End</span></h6></div>
+                        <div className="article-body competency">
+
+                            <div id="react-tinymce-2" className=" mce-content-body">
+                                <ul>
+                                    <li>Node, NPM</li>
+                                    <li>Express, Sails.js</li>
+                                    <li>PHP</li>
+                                    <li>Joomla, Wordpress</li>
+                                    <li>Zend</li>
+                                    <li>SQL</li>
+                                    <li>MongoDB</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="clearfix"></div>
+                    </article>
+                    <article className="competency">
+                        <div className="article-headings">
+                            <h6>
+                                <span className="undefined needsclick">Miscellaneous Skills</span></h6></div>
+                        <div className="article-body competency">
+                            <div id="react-tinymce-3" className=" mce-content-body">
+                                <ul>
+                                    <li>Git/SVN</li>
+                                    <li>Test-Driven-Development</li>
+                                    <li>Unit Testing, Grunt, Webpack</li>
+                                    <li>Docker</li>
+                                    <li>Vagrant</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="clearfix"></div>
                     </article>
                 </section>
             </div>
